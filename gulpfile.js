403aadc4296e7eae28cb8f41b98185a21a25e9b4
Reza Faiz A. Rahman <rezafaizarahman@gmail.com>
2019-09-21 08:40:44 +0700

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,19 @@
-const {
-  src,
-  dest,
-  parallel
-} = require('gulp');
-const sketch = require('gulp-sketch');
-const rename = require('gulp-rename');
+var { gulp, src, dest, watch, series, parallel } = require("gulp");
+var sketch = require("gulp-sketch");
+var svgo = require("gulp-svgo");
 
-function exportSketch() {
-  return src('src/sketch/*.sketch')
-    .pipe(sketch({
-      export: 'slices',
-      formats: 'png',
-      scales: [1.0, 4.0]
-    }))
-    .pipe(dest('dist/assets'))
-}
+var buildSVGs = function(done) {
+  return src(".src/sketch/*.sketch")
+    .pipe(
+      sketch({
+        export: "slices",
+        formats: "svg",
+        scales: "1.0",
+        saveForWeb: true
+      })
+    )
+    .pipe(svgo())
+    .pipe(dest(".dist/assets"));
+};
 
-exports.exportSketch = exportSketch;
+exports.svg = series(buildSVGs);
